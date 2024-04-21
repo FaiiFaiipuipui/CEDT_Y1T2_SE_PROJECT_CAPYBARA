49c230b1b12fb9cf97a50773e073e68a67c4de2d
BoondaPanich <nrboonda@gmail.com>
2024-04-21 23:26:13 +0700

    Add announcement route in server.js

--- backend/routes/campgrounds.js
+++ backend/routes/campgrounds.js
@@ -7,17 +7,17 @@ const {
   deleteCampground,
 } = require("../controllers/campgrounds");
 
-const appointmentRouter = require('./appointments');
-const weatherRouter = require('./weathers');
-const travelInfoRouter = require('./travelInfo');
+const appointmentRouter = require("./appointments");
+const weatherRouter = require("./weathers");
+const travelInfoRouter = require("./travelInfo");
 
 const router = express.Router();
 
 const { protect, authorize } = require("../middleware/auth");
 
-router.use('/:campgroundId/appointments/', appointmentRouter);
-router.use('/:campgroundId/weather/', weatherRouter);
-router.use('/:campgroundId/around/', travelInfoRouter);
+router.use("/:campgroundId/appointments/", appointmentRouter);
+router.use("/:campgroundId/weather/", weatherRouter);
+router.use("/:campgroundId/around/", travelInfoRouter);
 
 router
   .route("/")
