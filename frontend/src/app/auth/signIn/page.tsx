a22f8eb91ce95eea636374e6927e70c9d22aacfe
pccherrii <panisara.chk@gmail.com>
2024-04-24 15:40:04 +0700

    (loginPage)fix: edit function name to uppercase

--- frontend/src/app/auth/signIn/page.tsx
+++ frontend/src/app/auth/signIn/page.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { signIn } from "next-auth/react";
 import Link from "next/link";
 
-export default function signInPage() {
+export default function SignInPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
