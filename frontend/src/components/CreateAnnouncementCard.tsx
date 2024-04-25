799c6227c09e5b53731b862a3be92d539d2633a2
Shama Wachanachai <6633053521@student.chula.ac.th>
2024-04-25 15:02:16 +0700

    Fix Typo

--- frontend/src/components/CreateAnnouncementCard.tsx
+++ frontend/src/components/CreateAnnouncementCard.tsx
@@ -58,7 +58,7 @@ export default function CreateAnnouncementCard(){
                 ></input>
             </div>
         </div>
-        <textarea className="text-sm max-w-lg min-w-sm min-h-14 w-full border rounded-md p-2 bg-gray-100 border-1 border-cadetblue mt-4 mb-4" title="textArea" placeholder="Please enter your annoucement here"></textarea>
+        <textarea className="text-sm max-w-lg min-w-sm min-h-14 w-full border rounded-md p-2 bg-gray-100 border-1 border-cadetblue mt-4 mb-4" title="textArea" placeholder="Please enter your announcement here"></textarea>
         <div className="flex flex-wrap">
         <div className="flex flex-row right-0">
             <div id="noEndDateCheckbox" className="flex flex-row justify-start items-center">
