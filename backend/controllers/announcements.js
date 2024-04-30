e661e47220e55e04ad71afca9d9d070e057b61d3
FaiiFaiipuipui <pakarnpahulo2003@gmail.com>
2024-04-30 17:14:41 +0700

    Fix date validation in createAnnouncement function

--- backend/controllers/announcements.js
+++ backend/controllers/announcements.js
@@ -71,8 +71,12 @@ exports.getAnnouncement = async (req, res, next) => {
 // @access:  Private
 exports.createAnnouncement = async (req, res, next) => {
   try {
-    if (req.body.endDate < req.body.startDate) {
+    if (Date.parse(req.body.endDate) < Date.parse(req.body.startDate)) {
       return res.status(401).json({ success: false, message: "End date's time must be after start date's time" });
+    }
+
+    if (Date.parse(req.body.startDate) <=  Date.now() - (Date.now() % (86400 * 1000))) {
+      return res.status(401).json({ success: false, message: "start date's must be today or after" });
     }
 
     const announcement = await Announcement.create(req.body);
