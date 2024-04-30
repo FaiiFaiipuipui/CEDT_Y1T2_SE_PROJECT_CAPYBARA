6ce2910cc0c8fb7a1a3f883b08e166e1d40cf960
Shama Wachanachai <6633053521@student.chula.ac.th>
2024-04-30 23:04:23 +0700

    Refactor Cypress test for getting announcements and update campground card styling

--- frontend/cypress/e2e/getAnnoucement.cy.ts
+++ frontend/cypress/e2e/getAnnoucement.cy.ts
@@ -1,57 +1,18 @@
 describe('Test getting announcements', () => {
-  // it('Get all announcements', () => {
-  //   cy.visit('http://localhost:3000');
+  it('Get all announcements', () => {
+    cy.visit('http://localhost:3000');
 
-  //   cy.get('button').contains('SIGN IN').click();
+    cy.get('button').contains('SIGN IN').click();
 
-  //   cy.get('input[name=email]').type('admin@gmail.com')
-  //   cy.get('input[name=password]').type('admin1234')
-  //   cy.get('button').contains('Login').click();
-  //   cy.url().should('eq', 'http://localhost:3000/');
+    cy.get('input[name=email]').type('admin@gmail.com')
+    cy.get('input[name=password]').type('admin1234')
+    cy.get('button').contains('Login').click();
+    cy.url().should('eq', 'http://localhost:3000/');
 
-  //   cy.get('button').contains('CAMPGROUND').click();
+    cy.get('button').contains('CAMPGROUND').click();
 
-  //   cy.get('#announcement-card').should('have.length.greaterThan', 0);
-  // });
-
-  // it('Get announcements for each campground', () => {
-  //   cy.visit('http://localhost:3000');
-  
-  //   cy.get('button').contains('SIGN IN').click();
-  
-  //   cy.get('input[name=email]').type('admin@gmail.com')
-  //   cy.get('input[name=password]').type('admin1234')
-  //   cy.get('button').contains('Login').click();
-  //   cy.url().should('eq', 'http://localhost:3000/');
-  
-  //   cy.get('button').contains('CAMPGROUND').click();
-  
-  //   cy.get('.campground-card').then(($cards) => {
-  //     const totalCards = $cards.length;
-  
-  //     for (let i = 0; i < totalCards; i++) {
-  //       cy.get('.campground-card').eq(i).then(($card) => {
-
-  //         cy.wrap($card).click();
-  //         cy.url().should('not.eq', 'http://localhost:3000/campground');
-
-  //         const campgroundName = $card.find('.campground-name').text();
-  //         cy.log('Campground name: ', campgroundName);
-
-  //         // cy.log(`${cy.get(".announcement-small-card").should("have.length.greaterThan", 0)}`)
-  //         if (cy.get(".announcement-small-card").should("not.exist")) {
-  //           cy.get('.announcement-small-card').then(($announcementCard) => {
-  //             const totalAnnouncements = $announcementCard.length;
-  //             cy.log('Total announcements: ', totalAnnouncements);
-  //           });
-  //         }
-
-  //         cy.go('back');
-  //         cy.url().should('eq', 'http://localhost:3000/campground');
-  //       });
-  //     }
-  //   });
-  // });
+    cy.get('#announcement-card').should('have.length.greaterThan', 0);
+  });
 
   it('Announcements not exist in that campground', () => {
     cy.visit('http://localhost:3000');
